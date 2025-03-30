ba40823b8fbebc6a8ce94e47b4ada563e079e592
ravinu <ravinuh@proton.me>
2025-03-30 23:10:20 +0530

    Update Firebase Admin SDK credential path for render

--- src/firebase_config.py
+++ src/firebase_config.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Initialize Firebase Admin
-cred = credentials.Certificate('webuu/src/markzui-firebase-adminsdk-fbsvc-9b679ece1f.json')
+cred = credentials.Certificate('/etc/secrets/markzui-firebase-adminsdk-fbsvc-9b679ece1f.json')
 firebase_admin.initialize_app(cred)
 
 # Get Firestore client
